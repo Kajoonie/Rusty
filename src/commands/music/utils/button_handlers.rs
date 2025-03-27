a990a2cb69b1a83d00a8cdb410d4692d38e3f4e3
Charles <charles.t.cheney@gmail.com>
2025-03-27 09:55:31 -0700

    refactor repeated 'CreateInteractionResponse' code

--- src/commands/music/utils/button_handlers.rs
+++ src/commands/music/utils/button_handlers.rs
@@ -21,17 +21,12 @@ pub async fn handle_button_interaction(
 
     // Ensure we're in a call
     if let Err(err) = MusicManager::get_call(ctx, guild_id).await {
-        interaction
-            .create_response(
-                ctx,
-                serenity::CreateInteractionResponse::Message(
-                    CreateInteractionResponseMessage::default()
-                        .content("")
-                        .embed(embedded_messages::bot_not_in_voice_channel(err.into())),
-                ),
-            )
-            .await?;
-        return Ok(());
+        return create_response(
+            ctx,
+            interaction,
+            embedded_messages::bot_not_in_voice_channel(err.into()),
+        )
+        .await;
     }
 
     // Get the current track
@@ -46,71 +41,28 @@ pub async fn handle_button_interaction(
 
                 if is_playing {
                     track.pause()?;
-                    interaction
-                        .create_response(
-                            ctx,
-                            serenity::CreateInteractionResponse::Message(
-                                CreateInteractionResponseMessage::default()
-                                    .content("")
-                                    .embed(embedded_messages::paused(&metadata)),
-                            ),
-                        )
-                        .await?;
+                    create_response(ctx, interaction, embedded_messages::paused(&metadata)).await?;
                 } else {
                     track.play()?;
-                    interaction
-                        .create_response(
-                            ctx,
-                            serenity::CreateInteractionResponse::Message(
-                                CreateInteractionResponseMessage::default()
-                                    .content("")
-                                    .embed(embedded_messages::resumed(&metadata)),
-                            ),
-                        )
-                        .await?;
+                    create_response(ctx, interaction, embedded_messages::resumed(&metadata)).await?;
                 }
 
                 // Update button states
                 update_message_components(ctx, &mut *interaction.message, !is_playing, has_queue)
                     .await?;
             } else {
-                interaction
-                    .create_response(
-                        ctx,
-                        serenity::CreateInteractionResponse::Message(
-                            CreateInteractionResponseMessage::default()
-                                .content("")
-                                .embed(embedded_messages::no_track_playing()),
-                        ),
-                    )
-                    .await?;
+                create_response(ctx, interaction, embedded_messages::no_track_playing()).await?;
             }
         }
         "music_stop" => {
             if let Some((track, _)) = current_track {
                 track.stop()?;
-                interaction
-                    .create_response(
-                        ctx,
-                        serenity::CreateInteractionResponse::Message(
-                            CreateInteractionResponseMessage::new()
-                                .embed(embedded_messages::stopped(false)),
-                        ),
-                    )
-                    .await?;
+                create_response(ctx, interaction, embedded_messages::stopped(false)).await?;
 
                 // Update button states
                 update_message_components(ctx, &mut *interaction.message, false, false).await?;
             } else {
-                interaction
-                    .create_response(
-                        ctx,
-                        serenity::CreateInteractionResponse::Message(
-                            CreateInteractionResponseMessage::new()
-                                .embed(embedded_messages::no_track_playing()),
-                        ),
-                    )
-                    .await?;
+                create_response(ctx, interaction, embedded_messages::no_track_playing()).await?;
             }
         }
         "music_skip" => {
@@ -118,46 +70,41 @@ pub async fn handle_button_interaction(
                 let track_info = track.get_info().await?;
                 let is_playing = track_info.playing == PlayMode::Play;
                 track.stop()?;
-                interaction
-                    .create_response(
-                        ctx,
-                        serenity::CreateInteractionResponse::Message(
-                            CreateInteractionResponseMessage::new()
-                                .embed(embedded_messages::skipped(&metadata)),
-                        ),
-                    )
-                    .await?;
+                create_response(ctx, interaction, embedded_messages::skipped(&metadata)).await?;
 
                 // Update button states after a short delay to allow the next track to start
                 sleep(Duration::from_millis(100)).await;
                 update_message_components(ctx, &mut *interaction.message, is_playing, has_queue)
                     .await?;
             } else {
-                interaction
-                    .create_response(
-                        ctx,
-                        serenity::CreateInteractionResponse::Message(
-                            CreateInteractionResponseMessage::new()
-                                .embed(embedded_messages::no_track_to_skip()),
-                        ),
-                    )
-                    .await?;
+                create_response(ctx, interaction, embedded_messages::no_track_to_skip()).await?;
             }
         }
         _ => {
             error!("Unknown button ID: {}", interaction.data.custom_id);
-            interaction
-                .create_response(
-                    ctx,
-                    serenity::CreateInteractionResponse::Message(
-                        CreateInteractionResponseMessage::new()
-                            .embed(embedded_messages::not_pausable()),
-                    ),
-                )
-                .await?;
+            create_response(ctx, interaction, embedded_messages::not_pausable()).await?;
         }
     }
 
+    Ok(())
+}
+
+/// Create a response for the interaction
+async fn create_response(
+    ctx: &Context,
+    interaction: &mut ComponentInteraction,
+    embed: CreateEmbed,
+) -> Result<(), serenity::Error> {
+    interaction
+        .create_response(
+            ctx,
+            serenity::CreateInteractionResponse::Message(
+                CreateInteractionResponseMessage::default()
+                    .content("")
+                    .embed(embed),
+            ),
+        )
+        .await?;
     Ok(())
 }
 
